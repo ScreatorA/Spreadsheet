895afff95ed536d86fce573199786274579610c7
Sugirthan Arulsothy <sugirthan.arulsothy.programmer@gmail.com>
2025-02-20 15:46:23 +0100

    Finally, make your rangeRegex global and case-insensitive.

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const charRange = (start, end) =>
 
 const evalFormula = (x, cells) => {
   idToText = (id) => cells.find((cell) => cell.id === id).value;
-  const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/
+  const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi
 };
 
 window.onload = () => {
